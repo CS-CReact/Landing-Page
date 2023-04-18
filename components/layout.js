2b262d0c5a1effe5c7ece92034c0168a7495aca2
michellesylee <michellebellelee@yahoo.com>
2023-04-18 11:02:59 -0700

    draft of page

---
+++ components/layout.js
@@ -0,0 +1,76 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+const name = "C-React";
+export const siteTitle = "C-React";
+
+export default function Layout({ children, home }) {
+  return (
+    <>
+      <Navbar />
+      <div className={styles.container}>
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Learn how to build a personal website using Next.js"
+          />
+          <meta
+            property="og:image"
+            content={`https://og-image.vercel.app/${encodeURI(
+              siteTitle
+            )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+        </Head>
+        <header className={styles.header}>
+          {home ? (
+            <>
+              <Image
+                priority
+                src="/images/c-react.jpg"
+                className={utilStyles.borderCircle}
+                height={144}
+                width={144}
+                alt=""
+              />
+              <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            </>
+          ) : (
+            <>
+              <Link href="/">
+                <Image
+                  priority
+                  src="/images/profile.jpg"
+                  className={utilStyles.borderCircle}
+                  height={108}
+                  width={108}
+                  alt=""
+                />
+              </Link>
+              <h2 className={utilStyles.headingLg}>
+                <Link href="/" className={utilStyles.colorInherit}>
+                  {name}
+                </Link>
+              </h2>
+            </>
+          )}
+        </header>
+        <main>{children}</main>
+        {!home && (
+          <div className={styles.backToHome}>
+            <Link href="/">← Back to home</Link>
+          </div>
+        )}
+      </div>
+      <Footer />
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};

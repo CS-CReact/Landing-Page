a83e964df42303e5bc6a067495b507b1ad2a1c20
Martin Carmona <martin.e.carmona@gmail.com>
2023-04-20 09:27:59 -0700

    fixed prettier

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -8,7 +8,7 @@ import Layout from '../../components/layout';
 export default function FirstPost() {
   return (
     <Layout>
-     		 <Head>
+      <Head>
         <title>First Post</title>
       </Head>
       <h1>First Post</h1>

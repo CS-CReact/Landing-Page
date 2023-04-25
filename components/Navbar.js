0c01a9e7e7732b11e5e548d96f0441ddc7ff9143
ChTiSh <chentina.shen@gmail.com>
2023-04-25 12:48:53 -0400

    adjust navbar for screen size smaller than 280pixel-galaxy fold user

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,9 +5,9 @@ import 'tailwindcss/tailwind.css';
 
 export default function Navbar() {
   return (
-    <div className="sm:mx-5 phone:px-5 md:mx-8 md:px-5 px-10 pt-5 lg:mx-20">
+    <div className="px-1 mx-2 phone:px-5 md:mx-8 md:px-5 sm:px-7 pt-5 lg:mx-20">
       <div className="flex justify-between items-center">
-        <button className="rounded-full text-dark dark:bg-dark text-sm dark:text-white px-5 lg:mx-5">C-React</button>
+        <button className="rounded-full text-dark dark:bg-dark text-sm dark:text-white phone:px-5 px-3 lg:mx-5">C-React</button>
         <div className="text-center pr-10">
           <ul className="sm:flex hidden sm:gap-1 md:gap-3 lg:gap-6 text-sm">
             <li>
@@ -27,7 +27,7 @@ export default function Navbar() {
             </li>
           </ul>
         </div>
-        <button className="rounded-full bg-dark text-white dark:bg-white text-xs dark:text-dark px-5 py-0.5 lg:mx-5">Dark Mode</button>
+        <button className="rounded-full bg-dark text-white dark:bg-white text-xs dark:text-dark phone:px-5 px-3 py-0.5 lg:mx-5">Dark Mode</button>
       </div>
     </div>
   );

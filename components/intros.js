8f17abb2bbd184ac0e3ac32f897cbc568d205856
ChTiSh <chentina.shen@gmail.com>
2023-04-26 15:45:27 -0400

    change the display sequence for smaller screens

--- components/intros.js
+++ components/intros.js
@@ -4,27 +4,25 @@ export default function intros(){
     return(
         <>
           <div className='flex flex-col gap-20 text-center  pt-20'>
-            <div className='desktop:text-start mt-2'>
+            <div className='mt-2'>
                 <div className='desktop:flex justify-center gap-5'>
                     <Image
                         src='/images/placeholder.gif'
                         alt='function intro of custom renderer'
                         width={300}
                         height={500}
-                        className='shadow-md rounded-lg'
+                        className='shadow-md rounded-lg block mx-auto desktop:mx-1'
                     />
-                    <div className='pl-15 pt-10'>
-                        <h5>Custom Renderer</h5>
-                        <div className='w-11 shadow-md'></div>
+                    <div className='pt-10'>
+                        <p className='text-2xl'>Custom Renderer</p>
                         <p>Custom renderer to capture rendering priorities</p>
                     </div>
                 </div>
             </div>
-            <div className='text-end mt-2'>
-                <div className='flex justify-center gap-5'>
-                    <div className='pr-15 pt-10'>
+            <div className='mt-2'>
+                <div className='desktop:flex justify-center gap-5'>
+                    <div className='pt-10 hidden desktop:block'>
                         <p className='text-2xl'>Custom Renderer</p>
-                        <div className='w-11 shadow-md'></div>
                         <p>Custom renderer to capture rendering priorities</p>
                     </div>
                     <Image
@@ -32,8 +30,12 @@ export default function intros(){
                         alt='function intro of custom renderer'
                         width={300}
                         height={500}
-                        className='shadow-md rounded-lg'
+                        className='shadow-md rounded-lg block mx-auto desktop:mx-1'
                     />
+                    <div className='pt-10 desktop:hidden'>
+                        <p className='text-2xl'>Custom Renderer</p>
+                        <p>Custom renderer to capture rendering priorities</p>
+                    </div>
                 </div>
             </div>
           </div>

339d5d3014926e576b5984a663aee993621a8e2a
ChTiSh <chentina.shen@gmail.com>
2023-05-10 17:11:42 -0400

    prettier fix for contact form

--- components/contact.js
+++ components/contact.js
@@ -22,23 +22,23 @@ export default function Contact() {
       .then((data) => {
         let element = document.getElementById('emailSent');
         console.log(data);
-        element.classList.add("fade-in-out");
-        element.style.visibility="visible"; 
+        element.classList.add('fade-in-out');
+        element.style.visibility = 'visible';
         setTimeout(function () {
-          element.classList.remove("fade-in-out");    
-          element.style.visibility="hidden"; 
-        },5000);
+          element.classList.remove('fade-in-out');
+          element.style.visibility = 'hidden';
+        }, 5000);
       })
       .catch((error) => {
         console.error(error);
         let element = document.getElementById('somethingWrong');
         console.log(data);
-        element.classList.add("fade-in-out");
-        element.style.display="flex";
+        element.classList.add('fade-in-out');
+        element.style.display = 'flex';
         setTimeout(function () {
-          element.classList.remove("fade-in-out");    
-          element.style.display="none"; 
-        },5000);
+          element.classList.remove('fade-in-out');
+          element.style.display = 'none';
+        }, 5000);
       });
     setName('');
     setEmail('');
@@ -92,24 +92,47 @@ export default function Contact() {
             </div>
           </form>
 
-          <div id="emailSent" class="bg-palegreen border-t-4 invisible  border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+          <div
+            id="emailSent"
+            class="bg-palegreen border-t-4 invisible  border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+            role="alert"
+          >
             <div class="flex">
-              <div class="py-1"><svg class="fill-current h-5 w-7 text-teal-500 mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z"/></svg></div>
+              <div class="py-1">
+                <svg
+                  class="fill-current h-5 w-7 text-teal-500 mr-3"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M20.285 2l-11.285 11.567-5.286-5.011-3.714 3.716 9 8.728 15-15.285z" />
+                </svg>
+              </div>
               <div>
-                <p class="font-bold mt-1">Your message has been sent!</p> 
+                <p class="font-bold mt-1">Your message has been sent!</p>
               </div>
             </div>
           </div>
 
-          <div id="somethingWrong" class="bg-red-100 hidden border-t-4 border-red-500 rounded-b text-teal-900 px-4 py-3 shadow-md" role="alert">
+          <div
+            id="somethingWrong"
+            class="bg-red-100 hidden border-t-4 border-red-500 rounded-b text-teal-900 px-4 py-3 shadow-md"
+            role="alert"
+          >
             <div class="flex">
-              <div class="py-1"><svg class="fill-current h-6 w-6 text-red-500 mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z"/></svg></div>
+              <div class="py-1">
+                <svg
+                  class="fill-current h-6 w-6 text-red-500 mr-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
+                </svg>
+              </div>
               <div>
                 <p class="font-bold mt-1">Something went wrong!</p>
               </div>
             </div>
           </div>
-
         </div>
       </div>
     </>
